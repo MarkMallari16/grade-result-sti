f584cbbd3a47ad5802272cf5de3343a7e46f6d89
MarkMallari16 <mallariftw01@gmail.com>
2025-01-04 14:18:40 +0800

    fix the layout

--- src/App.vue
+++ src/App.vue
@@ -28,37 +28,41 @@ const calculateGrade = () => {
     <p>Mark Mallari</p>
   </header>
 
-  <main  class="flex justify-center m-5 lg:m-0 gap-5">
-   <div class="bg-base-300 h-full w-full   lg:w-1/3 rounded-lg p-10">
-      <div>
+  <main >
+   <div class="flex flex-col justify-center items-center m-5 lg:m-0">
+ 
+      <div class="bg-base-300 h-full w-full   lg:w-1/3 rounded-lg p-10">
+          <div>
 
-      <label for="">Enter Grade in Prelim</label>
-      <input v-model="prelim" type="text" id="prelim" class="mt-1 block input input-bordered w-full">
-      </div>
+          <label for="prelim">Prelim</label>
+          <input v-model="prelim" type="text" id="prelim" class="mt-1 block input input-bordered w-full" placeholder="Enter Grade in Prelim">
+          </div>
 
-      <div class="mt-4">
-      <label for="">Enter Grade in Midterm</label>
-      <input v-model="midterm" type="text" id="midterm" class="mt-1 block input input-bordered w-full">
-      </div>
+          <div class="mt-4">
+          <label for="midterm">Midterm</label>
+          <input v-model="midterm" type="text" id="midterm" class="mt-1 block input input-bordered w-full" placeholder="Enter Grade in Midterm">
+          </div>
 
-      <div class="mt-4">
-      <label for="">Enter Grade in Pre-Final</label>
-      <input v-model="preFinal" type="text" id="preFinal" class="mt-1 block input input-bordered w-full">
-      </div>
+          <div class="mt-4">
+          <label for="preFinal">Pre-Final</label>
+          <input v-model="preFinal" type="text" id="preFinal" class="mt-1 block input input-bordered w-full" placeholder="Enter Grade in Pre-FInal">
+          </div>
 
-      <div class="mt-4">
-      <label for="">Enter Grade in Finals</label>
-        <input v-model="finals" type="text" id="finals" class="mt-1 block input input-bordered w-full">
-      </div>
+          <div class="mt-4">
+            <label label for="finals">Finals</label>
+            <input v-model="finals" type="text" id="finals" class="mt-1 block input input-bordered w-full" placeholder="Enter Grade in Finals">
+          </div>
 
-    <div class="mt-4">
-      <button @click="calculateGrade" class="uppercase font-black btn btn-primary w-full">Calculate</button>
+        <div class="mt-4">
+          <button @click="calculateGrade" class="uppercase font-black btn btn-primary w-full">Calculate</button>
+        </div>
+      
+
+
     </div>
-    
-   </div>
-
-   <div v-if="finalGrade !== null" class="bg-base-300 h-full w-full   lg:w-1/3 rounded-lg p-10">
+  <div v-if="finalGrade !== null" class="mt-10 bg-base-300 h-full w-full   lg:w-1/3 rounded-lg p-10">
     <p class="text-lg font-bold">Final Grade: {{ finalGrade }}</p>
+   </div>
    </div>
   </main>
 </template>
