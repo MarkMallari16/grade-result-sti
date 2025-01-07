0d30cfe9302c072a0f774567ef63f0c44d06003f
MarkMallari16 <mallariftw01@gmail.com>
2025-01-08 01:52:25 +0800

    add margin side of navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,21 +1,23 @@
 <template>
-  <div className="navbar bg-base-100">
-    <div className="flex-1">
-      <a className="btn btn-ghost text-xl font-black">Grades Calculator</a>
+  <nav class="mx-5">
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl font-black">Grades Calculator</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li><a>Home</a></li>
+          <li>
+            <details>
+              <summary>About</summary>
+              <ul className="bg-base-100 rounded-t-none p-2">
+                <li><a>Home</a></li>
+                <li><a>About</a></li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div className="flex-none">
-      <ul className="menu menu-horizontal px-1">
-        <li><a>Home</a></li>
-        <li>
-          <details>
-            <summary>About</summary>
-            <ul className="bg-base-100 rounded-t-none p-2">
-              <li><a>Home</a></li>
-              <li><a>About</a></li>
-            </ul>
-          </details>
-        </li>
-      </ul>
-    </div>
-  </div>
+  </nav>
 </template>
