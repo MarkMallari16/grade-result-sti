7010f7a06232565dea79d2957b3eb52efce355d4
MarkMallari16 <mallariftw01@gmail.com>
2025-01-11 00:08:15 +0800

    fix code structure

--- src/App.vue
+++ src/App.vue
@@ -119,7 +119,6 @@ const clearFields = () => {
   </header>
   <main>
     <div class="flex flex-col justify-center items-center m-5 lg:m-0">
-    
       <div class="bg-base-200 h-full w-full lg:w-1/2 rounded-lg px-6 lg:px-5 py-10">
         <h1 class="text-2xl font-bold mb-4">Calculate your Final Grade</h1>
         <div>
@@ -195,7 +194,7 @@ const clearFields = () => {
                 {{ errors.preFinal }}
               </p>
             </div>
-            <div class="w-full"> 
+            <div class="w-full">
               <label for="preFinalWeight" class="font-medium">Weight</label>
               <input
                 v-model="preFinalWeight"
